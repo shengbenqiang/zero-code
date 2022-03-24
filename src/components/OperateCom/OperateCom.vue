378abf88e46cd9cbc4856e17f619b3d3694c7e32
shengbenqiang <3122971186@qq.com>
2022-03-24 14:15:02 +0800

    first commit

---
+++ src/components/OperateCom/OperateCom.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="operate">运行等操作</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import "./Operate.css";
+
+export default defineComponent({
+  name: "OperateCom",
+});
+</script>
